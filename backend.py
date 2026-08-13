4b96fea89ba35565192e489d9cabb406591b0803
Arushi Ranjan <arushi.ranjan26@gmail.com>
2026-08-13 12:45:05 +0530

    Added streaming feature in chatbot

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph, START, END
 from langchain_mistralai import ChatMistralAI
 from typing import TypedDict, Annotated
 from dotenv import load_dotenv
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver # stores memory in RAM
 
@@ -31,3 +31,11 @@ graph.add_edge('chat_node', END)
 chatbot = graph.compile(checkpointer=checkpointer)
 
 
+# for message_chunk, metadata in chatbot.stream(
+#     {'messages':HumanMessage(content="Recipe of Litti Chokha")},
+#     config={'configurable': {'thread_id': 'thread-1'}},
+#     stream_mode="messages"
+# ):
+#     if message_chunk.content:
+#         print(message_chunk.content, end=" ", flush=True)
+# # generator object: has 2 components -> msh chunk, metadata
